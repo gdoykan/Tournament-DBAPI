0820a46645688a50b9d75098083d3b67597a7e0c
George Doykan <George Doykan>
2017-06-13 17:53:13 -0500

    First commit for tournament project

---
+++ tournament.sql
@@ -0,0 +1,26 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+DROP DATABASE IF EXISTS tounament;
+
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players (
+    id serial primary key,
+    name text,
+    wins int
+);
+
+CREATE TABLE matches (
+    id serial primary key,
+    player1 integer references players(id),
+    player2 integer references players(id)
+
+);
+
